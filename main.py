c8afff1f3a9779e79c6273eb64ca9908804502eb
barrelingmoose <N/A>
2024-06-10 22:05:30 -0500

    Adding pandas and testing to check for syntax issues

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import numpy as np
+import pandas as pd
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
+
+def sigmoid_derivative(x):
+    return sigmoid(x)*(1-sigmoid(x))
 
 class NeuralNetwork: 
     def __init__(self, x, y): 
@@ -14,3 +18,12 @@ class NeuralNetwork:
     def feedforward(self): 
         self.layer1 = sigmoid(np.dot(self.input, self.weights1))
         self.output = sigmoid(np.dot(self.layer1, self.weights2))
+
+    def backprop(self): 
+        d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
+
+data = [[0,0], [1,1]]
+data_frame = pd.DataFrame(data)
+objTest = NeuralNetwork(data_frame, data_frame)
+objTest.feedforward()
+objTest.backprop()
